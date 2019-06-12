88c73017fc25567ba7e489a0669a601168182648
HarryCandlish <hcnd90@gmail.com>
2019-06-13 10:55:43 +1200

    typeof

--- utilities.js
+++ utilities.js
@@ -1,39 +1,43 @@
-function getType (thing) {
+function getType(thing) {
+  return typeof thing;
 }
 
-function isNumber (thing) {
-}
+function isNumber(thing) {}
 
-function toNumber (str) {
-}
+function toNumber(str) {}
 
-function isStringNumber (str) {
-}
+function isStringNumber(str) {}
 
-function add (a, b) {
-}
+function add(a, b) {}
 
-function addStrings (a, b) {
-}
+function addStrings(a, b) {}
 
-function addStringsOrNumbers (a, b) {
-}
+function addStringsOrNumbers(a, b) {}
 
-function isEmail (str) {
-}
+function isEmail(str) {}
 
-function countIf (array, fn) {
-}
+function countIf(array, fn) {}
 
-function filter (array, fn) {
-}
+function filter(array, fn) {}
 
-function map (array, fn) {
-}
+function map(array, fn) {}
 
-function filterStringsWithCommas (str) {
-}
+function filterStringsWithCommas(str) {}
 
-function splitStringByCommas (str) {
-}
+function splitStringByCommas(str) {}
 
+module.exports = {
+  getType: getType,
+  isNumber: isNumber,
+  toNumber: toNumber,
+  isStringNumber: isStringNumber,
+  add: add,
+  addStrings: addStrings,
+  addStringsOrNumbers: addStringsOrNumbers,
+  isEmail: isEmail,
+  countIf: countIf,
+  filter: filter,
+  map: map,
+  filterStringsWithCommas: filterStringsWithCommas,
+  splitStringByCommas: splitStringByCommas
+};
